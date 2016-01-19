c8b434462753225d54b37e6003324cf2c52b824b
rajendramedarametla <raja.java33@gmail.com>
2016-01-18 19:57:37 -0600

    full java class

--- javamodule/src/JavaTest.java
+++ javamodule/src/JavaTest.java
@@ -2,6 +2,32 @@
  * Created by rmedarametla on 1/18/2016.
  */
 public class JavaTest {
+    private String raj;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        JavaTest javaTest = (JavaTest) o;
+
+        return raj != null ? raj.equals(javaTest.raj) : javaTest.raj == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        return raj != null ? raj.hashCode() : 0;
+    }
+
+    public String getRaj() {
+        return raj;
+    }
+
+    public void setRaj(String raj) {
+        this.raj = raj;
+    }
+
     public static void main(String[] args){
         System.out.println("java output in console");
         System.out.println("java output in console just commit,not push");
