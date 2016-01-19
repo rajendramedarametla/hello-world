8667e8ec02e737ff8276a7f177ecb4f346b5dc24
rajendramedarametla <raja.java33@gmail.com>
2016-01-18 19:43:51 -0600

    java class added

---
+++ javamodule/src/JavaTest.java
@@ -0,0 +1,8 @@
+/**
+ * Created by rmedarametla on 1/18/2016.
+ */
+public class JavaTest {
+    public static void main(String[] args){
+        System.out.println("java output in console");
+    }
+}
